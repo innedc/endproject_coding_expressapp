7773d963f2b23fd5b601f9590c2cae4764ee020d
Inne dc <innedeceuster31@gmail.com>
2024-12-11 11:53:32 +0100

    cafe added

--- models/cafe.js
+++ models/cafe.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const cafeSchema = new mongoose.Schema({
     title: { type: String, required: true }, 
-    rating: { type: String, required: true }, 
+    rating: { type: Number, required: true }, 
+    price: { type: String, required: true }, 
     location: { type: String, required: true }, 
     hours: { type: String, required: true }, 
     createAt: { type: Date, default: Date.now },
 });
 
 //create a message model
-const User = mongoose.model('User', userSchema);
+const Cafe = mongoose.model('Cafe', cafeSchema);
 
-export default User;
+export default Cafe;
